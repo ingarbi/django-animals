5e1bd183f0f9f6ac183bb0644c7f0e798baa7247
ingarbi <104501854+ingarbi@users.noreply.github.com>
2022-10-02 22:02:06 +0300

    Init Version

--- wild_world/settings.py
+++ wild_world/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = config('SECRET_KEY')
+SECRET_KEY = SECRET_KEY= 'django-insecure-3&$a74971#k*kiy0208wf3%_(gwt06gop#h-zth%0a-u*i7hlv'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
