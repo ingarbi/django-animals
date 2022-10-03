0724b0811a7663d770c58489ca2a2df95b007ad9
ingarbi <104501854+ingarbi@users.noreply.github.com>
2022-10-03 19:58:04 +0300

    Railway Versio 11

--- wild_world/urls.py
+++ wild_world/urls.py
@@ -32,6 +32,6 @@ if settings.DEBUG:
         path('__debug__/', include(debug_toolbar.urls)),
     ] + urlpatterns
 
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 handler404 = pageNotFound
