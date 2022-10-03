e13b04b929f65be9734551023e988389d98de1f3
ingarbi <104501854+ingarbi@users.noreply.github.com>
2022-10-03 21:30:22 +0300

    Railway Versio 15

--- wild_world/settings.py
+++ wild_world/settings.py
@@ -138,7 +138,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
@@ -146,7 +147,7 @@ STATICFILES_DIRS = [
     
 
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
-
+STATIC_DIR = BASE_DIR / 'static'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
 
