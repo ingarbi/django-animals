d4e2bc80af8b51e877f981a261540e0a86647192
ingarbi <104501854+ingarbi@users.noreply.github.com>
2022-10-03 23:18:50 +0300

    Railway Versio 23

--- wild_world/urls.py
+++ wild_world/urls.py
@@ -38,10 +38,10 @@ if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 else:
     urlpatterns += [
-        url(f'^{settings.MEDIA_URL.lstrip("/")}(?<path>.*)$',
+        url(f'^{settings.MEDIA_URL.lstrip("/")}(?P<path>.*)$',
             mediaserve, {'document_root': settings.MEDIA_ROOT}),
-        url(f'^{settings.STATIC_URL.lstrip("/")}(?<path>.*)$',
+        url(f'^{settings.STATIC_URL.lstrip("/")}(?P<path>.*)$',
             mediaserve, {'document_root': settings.STATIC_ROOT}),
     ]
-
+    
 handler404 = pageNotFound
