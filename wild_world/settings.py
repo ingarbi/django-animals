0a4220558e0c4e6f2ae1fc277c25827b19f5ebef
ingarbi <104501854+ingarbi@users.noreply.github.com>
2022-10-03 21:48:36 +0300

    Railway Versio 18

--- wild_world/settings.py
+++ wild_world/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1', 'web-production-a5d8.up.railway.app']
+ALLOWED_HOSTS = ['web-production-a5d8.up.railway.app']
 
 
 # Application definition
