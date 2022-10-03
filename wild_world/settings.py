787528b198ca93a6959e7438be531df9608fbc60
ingarbi <104501854+ingarbi@users.noreply.github.com>
2022-10-03 19:25:21 +0300

    Railway Versio 9

--- wild_world/settings.py
+++ wild_world/settings.py
@@ -149,6 +149,8 @@ STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
 
+
+CSRF_TRUSTED_ORIGINS = ['https://*.https://web-production-a5d8.up.railway.app/','https://*.127.0.0.1']
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
